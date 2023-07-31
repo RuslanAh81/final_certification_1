5d1a5c594e63583811992ff28b6dd43258819fb2
Ruslan <arhara81@gmail.com>
2023-07-31 10:23:02 +0300

    new files added

--- data/config.py
+++ data/config.py
@@ -2,7 +2,6 @@ db_config = {
     "mysql": {
         "host": "localhost",
         "user": "root",
-        "pass": "ruslan",
-        "db_name": "Human_friends"
+        "pass": "ruslan"
     }
 }
